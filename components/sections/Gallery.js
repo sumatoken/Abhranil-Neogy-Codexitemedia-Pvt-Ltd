2940e918bb018e1184c0abf74b71dc5f475a916d
Sumato Ken <bermime1@gmail.com>
2022-08-19 16:29:27 +0100

    added gallery

--- components/sections/Gallery.js
+++ components/sections/Gallery.js
@@ -1,26 +1,5 @@
-import { Grid, List, ListItem, Typography } from "@mui/material";
-import Image from "next/image";
 import React from "react";
-import styles from "../../styles/Home.module.scss";
 
 export default function Gallery() {
-  const numberOfImagesArrayLoop = Array.from(Array(18).keys());
-  return (
-    <>
-      <Grid
-        id="gallery"
-        container
-        spacing={1}
-        sx={{ background: "#F1E5AC", display: "flex" }}
-        alignItems="center"
-        justifyContent="center"
-      >
-        <SwipeableViews>
-          {numberOfImagesArrayLoop.map((n, key) => (
-            <Image src={`/gallery-${key + 1}`} width={400} height={500} />
-          ))}
-        </SwipeableViews>
-      </Grid>
-    </>
-  );
+  return <div>Gallery</div>;
 }
