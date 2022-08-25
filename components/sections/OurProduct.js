8975aff6d7f5c9eabb0b4c75a2045df149d8e8c5
XMTA toshiba <bermime1@gmail.com>
2022-08-25 18:02:49 +0100

    working on quick links

--- components/sections/OurProduct.js
+++ components/sections/OurProduct.js
@@ -6,7 +6,9 @@ export default function OurProduct() {
   return (
    <>
       <span id="product"></span>
-      <h5 className={styles.subheading} sytle={{backgroundColor: "#F1E5AC"}}>Our Products</h5>
+      <div className="bg-[#F1E5AC]">
+      <h5 className={styles.subheading} ><b>Our Products</b></h5>
+      </div>
     <div className="w-full flex flex-col md:flex-row lg:flex-row xl:flex-row">
       <div className="w-full p-4 rounded bg-[#F1E5AC] flex flex-col md:w-1/2 lg:width-1/2 xl:width-1/2">
         <span className={styles.subtitle}>
