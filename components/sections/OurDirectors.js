f74a1f49ee3fc1e380633c114c38cf5f8822d6e5
XMTA toshiba <bermime1@gmail.com>
2022-08-25 19:33:56 +0100

    directors

--- components/sections/OurDirectors.js
+++ components/sections/OurDirectors.js
@@ -8,7 +8,7 @@ import Link from "next/link";
 import Image from "next/image";
 export default function OurDirectors() {
   return (
-    <div className="flex flex-col md:flex-row lg:flex-row   ">
+    <div className="flex flex-col md:flex-row lg:flex-row   mt-4">
       <div
         className="w-full flex flex-col border rounded-lg border-yellow-300 p-4"
         style={{
--- components/sections/OurProduct.js
+++ components/sections/OurProduct.js
@@ -13,7 +13,7 @@ export default function OurProduct() {
         </h5>
       </div>
       <div className="w-full flex flex-col md:flex-row lg:flex-row xl:flex-row">
-        <div className="w-full p-4 m-1 rounded bg-[#F1E5AC] flex flex-col md:w-1/2 lg:width-1/2 xl:width-1/2">
+        <div className="w-full p-4 mr-1 rounded bg-[#F1E5AC] flex flex-col md:w-1/2 lg:width-1/2 xl:width-1/2">
           <span className={styles.subtitle}>
             <b className="text-black"> Malaysian Tropical Timber Product</b>
           </span>
@@ -50,7 +50,7 @@ export default function OurProduct() {
             compliance to the MTIB specifications.
           </div>
         </div>
-        <div className="w-full p-4 m-1 rounded bg-[#F1E5AC] flex flex-col md:w-1/2 lg:width-1/2 xl:width-1/2">
+        <div className="w-full p-4 ml-1 rounded bg-[#F1E5AC] flex flex-col md:w-1/2 lg:width-1/2 xl:width-1/2">
           <span className={styles.subtitle}>
             {" "}
             <b className="text-black">Palm Tree Based Products </b>
