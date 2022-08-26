a5e2b9914af102e1c0560f349b39447382ba94b2
XMTA toshiba <bermime1@gmail.com>
2022-08-26 15:57:59 +0100

    positioning sections

--- components/sections/Chart.js
+++ components/sections/Chart.js
@@ -21,7 +21,7 @@ const Card = ({ title, employee }) => {
 export default function Chart() {
   return (
     <div className={styles.description} style={{ background: "#F1E5AC",
-    paddingTop: "90px",
+    paddingTop: "110px",
       }}>
       
       <div id="chart" className="z-10 flex flex-col gap-5 items-center">
