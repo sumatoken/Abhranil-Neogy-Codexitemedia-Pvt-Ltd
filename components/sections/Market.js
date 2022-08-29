fe0b483b75e49986babd28cb61cead71446f48a2
Sumato Ken <bermime1@gmail.com>
2022-08-29 17:41:18 +0100

    added a break to export section title

--- components/sections/Market.js
+++ components/sections/Market.js
@@ -30,9 +30,9 @@ export default function Market() {
     "Maldives",
   ];
   return (
-    
     <div className="w-full pt-[60px]">
       <h5 className={styles.subheading}>Export Market</h5>
+      <br />
       <div className="w-full grid grid-cols-2 md:grid-cols-4 content-center">
         {countries.map((country, key) => (
           <div key={key} className="flex flex-col items-center">
